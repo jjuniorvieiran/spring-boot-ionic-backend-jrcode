5de498c38c4535ef2a349cc7500bdcfab3767f09
Júnior <junior.jaava@gmail.com>
2019-07-20 17:08:52 +0200

    Commiting service and repository lawers

---
+++ cursomc/src/main/java/com/juniorvieira/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.juniorvieira.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.juniorvieira.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
---
+++ cursomc/src/main/java/com/juniorvieira/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.juniorvieira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.juniorvieira.cursomc.domain.Categoria;
+import com.juniorvieira.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
