e81100565a7422d6edf56ee51acdeb78f663bdb1
Júnior <junior.jaava@gmail.com>
2019-07-28 15:47:57 +0200

    Implementing validation in Categoria

--- cursomc/src/main/java/com/juniorvieira/cursomc/services/CategoriaService.java
+++ cursomc/src/main/java/com/juniorvieira/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.juniorvieira.cursomc.domain.Categoria;
+import com.juniorvieira.cursomc.dto.CategoriaDTO;
 import com.juniorvieira.cursomc.repositories.CategoriaRepository;
 import com.juniorvieira.cursomc.resource.exception.DataIntegrityException;
 import com.juniorvieira.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 
 }
