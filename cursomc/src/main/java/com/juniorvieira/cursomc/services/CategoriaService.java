6af865e45b0122725320fb8428b64c2e3b2d3bf7
Júnior <junior.jaava@gmail.com>
2019-07-28 15:23:43 +0200

    Working with pagination. Delivering resource and service class

--- cursomc/src/main/java/com/juniorvieira/cursomc/services/CategoriaService.java
+++ cursomc/src/main/java/com/juniorvieira/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.juniorvieira.cursomc.domain.Categoria;
@@ -20,11 +23,10 @@ public class CategoriaService {
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! "
-				+ " Id: " + id + ", Tipo: " + Categoria.class.getName())
-		);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! " + " Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
@@ -39,14 +41,18 @@ public class CategoriaService {
 		find(id);
 		try {
 			repo.deleteById(id);
-		}
-		catch (DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
 	}
-	
+
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 
 }
