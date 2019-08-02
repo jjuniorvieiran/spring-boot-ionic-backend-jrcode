0dce3b001e1ff0467434b123d5394bd959ad8150
Júnior <junior.jaava@gmail.com>
2019-08-02 20:10:38 +0200

    Validataion email  using DB. Using Spring data to make the custom query (findBy)

--- cursomc/src/main/java/com/juniorvieira/cursomc/services/validation/ClienteInsertValidator.java
+++ cursomc/src/main/java/com/juniorvieira/cursomc/services/validation/ClienteInsertValidator.java
@@ -7,13 +7,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.juniorvieira.cursomc.domain.Cliente;
 import com.juniorvieira.cursomc.domain.enums.TipoCliente;
 import com.juniorvieira.cursomc.dto.ClienteNewDTO;
+import com.juniorvieira.cursomc.repositories.ClienteRepository;
 import com.juniorvieira.cursomc.resource.exception.FieldMessage;
 import com.juniorvieira.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -29,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
